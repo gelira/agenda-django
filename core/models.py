bd71d1e6a4c48903b862794c3d8ee25f8427af86
Geraldo Eloi <geraldo.eloi11@gmail.com>
2019-11-06 13:29:41 -0300

    Models Pessoa e Telefone

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class Telefone(models.Model):
+    numero = models.CharField(max_length=20)
+    pessoa = models.ForeignKey(
+        to=Pessoa,
+        on_delete=models.CASCADE,
+        related_name='telefone'
+    )
+
+    def __str__(self):
+        return self.numero
