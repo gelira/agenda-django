22ea2f52ff2fdfdf60040f5e2e21c04343f4cc1d
Geraldo Eloi <geraldo.eloi11@gmail.com>
2019-11-07 07:35:06 -0300

    Atualizar contato

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,7 @@ app_name = 'core'
 urlpatterns = [
     path('lista', views.ListaContatosView.as_view(), name='lista'),
     path('cadastro', views.CadastrarContatoView.as_view(), name='cadastro_contato'),
+    path('atualizar/<int:pk>', views.AtualizarContatoView.as_view(), name='atualizar_contato'),
     path('telefones/<int:pk>', views.ListaTelefonesView.as_view(), name='telefones'),
     path('telefones/<int:pk>/cadastro', views.CadastrarTelefoneView.as_view(), name='cadastro_telefone'),
     path('', views.InicioView.as_view(), name='inicio')
--- core/views.py
+++ core/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import reverse
-from django.views.generic import ListView, CreateView, TemplateView
+from django.urls import reverse_lazy
+from django.views import generic
 
 from . models import Pessoa, Telefone
 from . forms import PessoaForm, TelefoneForm
 from . mixins import GetPessoaMixin
 
-class InicioView(TemplateView):
+class InicioView(generic.TemplateView):
     template_name = 'core/inicio.html'
 
-class ListaContatosView(ListView):
+class ListaContatosView(generic.ListView):
     model = Pessoa
     template_name = 'core/lista_contatos.html'
     context_object_name = 'pessoas'
 
-class ListaTelefonesView(GetPessoaMixin, ListView):
+class ListaTelefonesView(GetPessoaMixin, generic.ListView):
     template_name = 'core/lista_telefones.html'
     context_object_name = 'telefones'
 
@@ -25,14 +26,14 @@ class ListaTelefonesView(GetPessoaMixin, ListView):
         ctx['pessoa'] = self.pessoa
         return ctx
 
-class CadastrarContatoView(CreateView):
+class CadastrarContatoView(generic.CreateView):
     form_class = PessoaForm
     template_name = 'core/form_contato.html'
 
     def get_success_url(self):
         return reverse('core:lista')
 
-class CadastrarTelefoneView(GetPessoaMixin, CreateView):
+class CadastrarTelefoneView(GetPessoaMixin, generic.CreateView):
     template_name = 'core/form_telefones.html'
     form_class = TelefoneForm
 
@@ -48,3 +49,9 @@ class CadastrarTelefoneView(GetPessoaMixin, CreateView):
 
     def get_success_url(self):
         return reverse('core:telefones', kwargs={'pk': self.kwargs['pk']})
+
+class AtualizarContatoView(generic.UpdateView):
+    model = Pessoa
+    form_class = PessoaForm
+    template_name = 'core/atualizar_contato.html'
+    success_url = reverse_lazy('core:lista')
