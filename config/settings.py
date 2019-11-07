e8c6314c52b389b73ab3ebbe687cabb20b8b4290
Geraldo Eloi <geraldo.eloi11@gmail.com>
2019-11-07 15:15:18 -0300

    Soft delete nos models

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'core',
+    'safedelete',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -108,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
--- core/models.py
+++ core/models.py
@@ -1,12 +1,25 @@
 from django.db import models
+from safedelete.models import SafeDeleteModel, SOFT_DELETE_CASCADE
 
-class Pessoa(models.Model):
+class TimestampedModel(SafeDeleteModel):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Pessoa(TimestampedModel):
+    _safedelete_policy = SOFT_DELETE_CASCADE
     nome = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nome
 
-class Telefone(models.Model):
+    class Meta:
+        db_table = 'pessoas'
+        ordering = ['nome', '-created']
+
+class Telefone(TimestampedModel):
     numero = models.CharField(max_length=20)
     pessoa = models.ForeignKey(
         to=Pessoa,
@@ -16,3 +29,7 @@ class Telefone(models.Model):
 
     def __str__(self):
         return self.numero
+
+    class Meta:
+        db_table = 'telefones'
+        ordering = ['numero', '-created']
